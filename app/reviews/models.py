d62e840726c6aad9944c8a44cd967e1b594d2321
Kevin Renkes <kevinrenkes@gmail.com>
2022-01-25 20:27:49 -0600

    changes to user .html templates

--- app/reviews/models.py
+++ app/reviews/models.py
@@ -10,4 +10,14 @@ class Review(models.Model):
     home_release_year = models.CharField(max_length=10, blank=False, default='')
     heavy_lang_index = models.CharField(max_length=100, blank=False, default='')
 
+class Movie(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, blank=False, default='')
+    title = models.CharField(max_length=70, blank=False, default='')
+    industry_rating = models.CharField(max_length=10, blank=False, default='')
+    theater_release_year = models.CharField(max_length=10, blank=True, null=True)
+    home_release_year = models.CharField(max_length=10, blank=False, default='')
+    heavy_lang_index = models.CharField(max_length=100, blank=False, default='')
+    light_lang_index = models.CharField(max_length=100, blank=False, default='')
+    racial_lang_index = models.CharField(max_length=100, blank=False, default='')
+
 # light_lang_index, religious_lang_index, racial_lang_index
