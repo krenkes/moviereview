697c2704114afc854dfc24d1104d7fc43382817e
Kevin Renkes <kevinrenkes@gmail.com>
2022-03-22 19:46:39 -0500

    comment filtered views

--- app/reviews/urls.py
+++ app/reviews/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', reviews_views.index.as_view(), name='home'),
-    path('filter/', reviews_views.list_filtered_reviews.as_view(), name='filter'),
+    # path('filter/', reviews_views.list_filtered_reviews.as_view(), name='filter'),
     path('api/reviews/', reviews_views.review_list),
     path('api/reviews/<int:pk>/', reviews_views.review_detail),
     path('api/reviews/filter/', reviews_views.review_list_filter)
