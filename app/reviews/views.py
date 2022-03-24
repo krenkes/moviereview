2a69515aaa8e3292c3c43903d6c974d014ddb25b
Kevin Renkes <kevinrenkes@gmail.com>
2022-03-24 15:21:41 -0500

    filter test

--- app/reviews/views.py
+++ app/reviews/views.py
@@ -112,14 +112,14 @@ def review_detail(request, pk):
 
 @api_view(['GET'])
 def review_list_filter(request):
-    # review_data = JSONParser().parse(request)
+    review_data = JSONParser().parse(request)
     # return JsonResponse(review_data, safe=False)
     # reviews = Review.objects.all()
     # reviews = reviews.filter(industry_rating = request.industry_rating)
 
     reviews = Review.objects.all()
 
-    industry_rating = request.GET.get('industry_rating', None)
+    industry_rating = review_data.GET.get('industry_rating', None)
     return JsonResponse(industry_rating, safe=False)
     # reviews= reviews.filter(industry_rating__icontains=industry_rating)
 
