a076f3e4cf700f01aaa62f9168500559532bc1a3
Kevin Renkes <kevinrenkes@gmail.com>
2022-03-25 10:47:41 -0500

    filter test again

--- app/reviews/views.py
+++ app/reviews/views.py
@@ -113,7 +113,8 @@ def review_detail(request, pk):
 @api_view(['GET'])
 def review_list_filter(request):
     review_data = JSONParser().parse(request)
-    return JsonResponse(review_data, safe=False)
+    data = {"hello": review_data}
+    return JsonResponse(data, safe=False)
     # reviews = Review.objects.all()
     # reviews = reviews.filter(industry_rating = review_data.data[industry_rating])
     
